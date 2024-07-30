0e314acb534385feda9e5b1bd435682b755baab3
Maico-Zurbriggen <zurbriggenmaico8@gmail.com>
2024-07-30 09:57:14 -0300

    Agregando variable de entorno al puerto

--- part3/index.js
+++ part3/index.js
@@ -90,7 +90,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${datos.length} people</p><p>${fecha}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
 })
